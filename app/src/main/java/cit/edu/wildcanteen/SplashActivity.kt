e29cca935870356e0d5652a0b6c74163f6afe2e1
jangkayl <villarazojk@gmail.com>
2025-03-18 23:28:24 +0800

    i think goods appcompact??

--- app/src/main/java/cit/edu/wildcanteen/SplashActivity.kt
+++ app/src/main/java/cit/edu/wildcanteen/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import android.widget.TextView
@@ -40,9 +41,10 @@ class SplashActivity : Activity() {
         splashText.startAnimation(textAnim)
         splashText2.startAnimation(textAnim2)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 2000)
+
     }
 }
